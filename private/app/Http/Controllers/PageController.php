4ff285c6de6e2b4e36c12ea8c836f3da697c542d
unknown <dev.ismail97@gmail.com>
2021-01-13 22:38:30 +0200

    remove {get-write-paid} from dynamic public

--- private/app/Http/Controllers/PageController.php
+++ private/app/Http/Controllers/PageController.php
@@ -18,32 +18,31 @@ class PageController extends Controller
      */
     public function show(string $slug)
     {
-        $page = Page::whereSlug($slug)->first();
 
-        if (!$page) {
+        if ( $slug == 'privacy' || $slug == 'terms' ){
+            
+            $page = Page::whereSlug($slug)->first();
+
+            if (!$page) {
+                abort(404);
+            }
+    
+            if ($page->status !== 1) {
+                abort(404);
+            }
+    
+            if ($slug != $page->slug) {
+                return redirect($page->permalink(), 301);
+            }
+    
+            return view('public.pages.show', [
+                'page' => $page,
+            ]);
+
+        }else{
             abort(404);
         }
 
-        if ($page->status !== 1) {
-            abort(404);
-        }
 
-        if ($slug != $page->slug) {
-            return redirect($page->permalink(), 301);
-        }
-
-        if (strpos($page->content, '[withdrawal_methods]') !== false) {
-            $withdrawal_methods = view('public._partials.withdrawal_methods')->render();
-            $page->content = str_replace('[withdrawal_methods]', $withdrawal_methods, $page->content);
-        }
-
-        if (strpos($page->content, '[payout_rates]') !== false) {
-            $payout_rates = view('public._partials.payout_rates')->render();
-            $page->content = str_replace('[payout_rates]', $payout_rates, $page->content);
-        }
-
-        return view('public.pages.show', [
-            'page' => $page,
-        ]);
     }
 }
