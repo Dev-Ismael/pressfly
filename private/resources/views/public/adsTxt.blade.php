092bd7f894f13928a48e54d435691a2cf7c6d76a
unknown <dev.ismail97@gmail.com>
2021-04-11 08:24:38 +0200

    fix adsTxt file

--- private/resources/views/public/adsTxt.blade.php
+++ private/resources/views/public/adsTxt.blade.php
@@ -1,9 +1,3 @@
-
-
 <pre style="word-wrap: break-word; white-space: pre-wrap;">
-#Ads.txt topiclix.com
-#Ads.txt managed by AdsTxtManager.com - the free ads.txt management tool for publishers
-      
 google.com, pub-3062309685670430, DIRECT, f08c47fec0942fa0
-      
 </pre>
