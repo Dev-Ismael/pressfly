3a7257ec7b4238060d47f4ca0ea473c889ba1117
Abdulrahman Ismael <64872640+Dev-Ismael@users.noreply.github.com>
2022-07-02 22:18:23 +0200

    add site name

--- private/resources/views/public/pages/terms.blade.php
+++ private/resources/views/public/pages/terms.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.front')
 
 @section('title', "Terms")
-@section('description', "topiclix terms")
+@section('description', "jourlive terms")
 
 @section('content')
     <main role="main" class="container">
@@ -13,18 +13,18 @@
             <h3>1.Intellectual Property Rights Infringement</h3>
             <p>It is our policy to respond to clear notices of alleged intellectual property rights infringement. Our Copyright Infringement Policy is designed to make submitting notices of alleged infringement to us as straightforward as possible while reducing the number of notices that we receive that are fraudulent or difficult to understand or verify. If you believe that your Intellectual Property Rights have been violated, please notify us on our Website and we will investigate.</p>
             <h3>2. Disclaimer</h3>
-            <p>The materials on topiclix's website are provided on an 'as is' basis. topiclix makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
-            <p>Further, topiclix does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</p>
+            <p>The materials on jourlive's website are provided on an 'as is' basis. jourlive makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
+            <p>Further, jourlive does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</p>
             <h3>3 . Limitations</h3>
-            <p>In no event shall topiclix or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on topiclix's website, even if topiclix or a topiclix authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+            <p>In no event shall jourlive or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on jourlive's website, even if jourlive or a jourlive authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
             <h3>4 . Intellectual Property Rights Infringement</h3>
             <p>It is our policy to respond to clear notices of alleged intellectual property rights infringement. Our Copyright Infringement Policy is designed to make submitting notices of alleged infringement to us as straightforward as possible while reducing the number of notices that we receive that are fraudulent or difficult to understand or verify. If you believe that your Intellectual Property Rights have been violated, please notify us on our Website and we will investigate.</p>
             <h3>5. Accuracy of materials</h3>
-            <p>The materials appearing on topiclix's website could include technical, typographical, or photographic errors. topiclix does not warrant that any of the materials on its website are accurate, complete or current. topiclix may make changes to the materials contained on its website at any time without notice. However topiclix does not make any commitment to update the materials.</p>
+            <p>The materials appearing on jourlive's website could include technical, typographical, or photographic errors. jourlive does not warrant that any of the materials on its website are accurate, complete or current. jourlive may make changes to the materials contained on its website at any time without notice. However jourlive does not make any commitment to update the materials.</p>
             <h3>6. Links</h3>
-            <p>topiclix has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by topiclix of the site. Use of any such linked website is at the user's own risk.</p>
+            <p>jourlive has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by jourlive of the site. Use of any such linked website is at the user's own risk.</p>
             <h3>7. Modifications</h3>
-            <p>topiclix may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
+            <p>jourlive may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
             <h3>8. Governing Law</h3>
             <p>These terms and conditions are governed by and construed in accordance with the laws of egypt and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
         </div>
