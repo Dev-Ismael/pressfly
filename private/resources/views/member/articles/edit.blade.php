e77c438ba917cffa663c8560f13a0779fe99b513
Abdulrahman Ismael <64872640+Dev-Ismael@users.noreply.github.com>
2022-08-13 21:51:07 +0200

    Access User Update all Article Before reviewing

--- private/resources/views/member/articles/edit.blade.php
+++ private/resources/views/member/articles/edit.blade.php
@@ -4,6 +4,26 @@
 
 @section('content')
 
+
+    <div class="article-status-alert">
+
+        @if ( $article->status == 1 )
+            <div class="alert alert-success" role="alert">
+                <i class="fas fa-check"></i>
+                &nbsp;
+                Your article is Approved & published... if You Edit it & save article will review again
+            </div>
+        @elseif( $article->status == 3 )
+            <div class="alert alert-warning status-box pendding" role="alert">
+                <i class="fas fa-spinner"></i>
+                &nbsp;
+                Your article is Still Pending... You can Edit it Before Reviewing
+            </div>
+        @endif
+    </div>
+
+
+
     <form method="post" action="{{ route('member.articles.update', $article->id) }}" enctype="multipart/form-data">
         @csrf
         @method('put')
@@ -53,70 +73,79 @@
                     @enderror
                 </div>
                 
-                <!------------- If isset need improvement ==> Image -------------->
-                @if( $article->review_messege == "bad_image" )
-                    <div class="form-group">
-                        {{ Form::label('upload_featured_image', __('Article Image')) }}
-                        <a href="{{ url($article->featuredImage->file) }}" target="_blank">
-                            <img src="{{ asset($articleFile->file) }}" alt="article-img"
-                            style="width:80px;border-radius:5px;margin:5px">
-                        </a>
-                        {{ Form::file('upload_featured_image', ['class' => 'form-control' , 'required' => true ]) }}
-                        @error('upload_featured_image')
-                            <p class="alert alert-danger mt-2">{{ $message }}</p>
-                        @enderror
-                    </div>
-                @elseif( $article->status == 3 )
-                    <div class="form-group">
-                        {{ Form::label('upload_featured_image', __('Article Image')) }}
-                        <a href="{{ url($article->featuredImage->file) }}" target="_blank">
-                            <img src="{{ asset($articleFile->file) }}" alt="article-img"
-                            style="width:80px;border-radius:5px;margin:5px">
-                        </a>
-                        {{ Form::file('upload_featured_image', ['class' => 'form-control' ]) }}
-                        @error('upload_featured_image')
-                            <p class="alert alert-danger mt-2">{{ $message }}</p>
-                        @enderror
-                    </div>
-                @else
-                   <!------ Show saved file ------->
-                    <div class="form-group row d-flex align-items-center">
-                        <label for="upload_featured_image" class="col-12 col-form-label text-center">
-                            @if($articleFile)
+
+
+                <div class="img-field">
+                    @if( $article->review_messege == "bad_image" )
+                        <!------------- If isset need improvement ==> Show Image Input -------------->
+                        <div class="form-group">
+                            {{ Form::label('upload_featured_image', __('Change Article Image')) }}
+                            <a href="{{ url($article->featuredImage->file) }}" target="_blank">
                                 <img src="{{ asset($articleFile->file) }}" alt="article-img"
-                                style="max-width: 100%;">
-                            @endif
-                        </label>
-                    </div>
-                @endif
+                                style="width:80px;border-radius:5px;margin:5px">
+                            </a>
+                            {{ Form::file('upload_featured_image', ['class' => 'form-control' , 'required' => true ]) }}
+                            @error('upload_featured_image')
+                                <p class="alert alert-danger mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    @elseif( $article->status == 3 )
+                        <!------------- If Article Still New Pending ==> Show Image Input -------------->
+                        <div class="form-group">
+                            {{ Form::label('upload_featured_image', __('Change Article Image')) }}
+                            <a href="{{ url($article->featuredImage->file) }}" target="_blank">
+                                <img src="{{ asset($articleFile->file) }}" alt="article-img"
+                                style="width:80px;border-radius:5px;margin:5px">
+                            </a>
+                            {{ Form::file('upload_featured_image', ['class' => 'form-control' ]) }}
+                            @error('upload_featured_image')
+                                <p class="alert alert-danger mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    @else
+                    <!------ Show saved file ------->
+                        <div class="form-group row d-flex align-items-center">
+                            <label for="upload_featured_image" class="col-12 col-form-label text-center">
+                                @if($articleFile)
+                                    <img src="{{ asset($articleFile->file) }}" alt="article-img"
+                                    style="max-width: 100%;">
+                                @endif
+                            </label>
+                        </div>
+                    @endif
+                </div>
                 
-                <!------------- If isset need improvement ==> Content -------------->
-                @if( $article->review_messege == "copied_content"  || $article->status == 3 )
-                    <div class="form-group">
-                        <label for="content">{{ __('Content') }}</label>
-                        <textarea id="content" name="content" class="form-control text-editor"
-                                >{{ old('content', $article_update->content) }}</textarea>
-                    </div>
-                    @error('content')
-                        <p class="alert alert-danger mt-2">{{ $message }}</p>
-                    @enderror
-                @else
-                    <!------ Show saved Content ------->
-                    <div class="form-group">
-                        <div class="container">
-                            <h3>Article Content</h3>
-                            <div class="article-content">
-                            {!! $article->getFinalContent() !!}
+                
+                <div class="content-field">
+                    <!------------- If isset need improvement ==> Content -------------->
+                    @if( $article->review_messege == "copied_content"  || $article->status == 3 )
+                        <div class="form-group">
+                            <label for="content">{{ __('Content') }}</label>
+                            <textarea id="content" name="content" class="form-control text-editor" required>{{ old('content', $article_update->content) }}</textarea>
+                        </div>
+                        @error('content')
+                            <p class="alert alert-danger mt-2">{{ $message }}</p>
+                        @enderror
+                    @else
+                        <!------ Show saved Content ------->
+                        <div class="form-group">
+                            <div class="container">
+                                <h3>Article Content</h3>
+                                <div class="article-content">
+                                {!! $article->getFinalContent() !!}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <!--------------- hidden content input ------------------>
-                    <div class="form-group d-none">
-                        <label for="content">{{ __('Content') }}</label>
-                        <textarea id="content" name="content" class="form-control text-editor"
-                                >{{ old('content', $article_update->content) }}</textarea>
-                    </div>
-                @endif
+                        <!--------------- hidden content input ------------------>
+                        <div class="form-group d-none">
+                            <label for="content">{{ __('Content') }}</label>
+                            <textarea id="content" name="content" class="form-control text-editor">{{ old('content', $article_update->content) }}</textarea>
+                        </div>
+                    @endif
+                </div>
+
+
+                
 
                 <div class="form-group d-none">
                     <label for="reason">{{ __('Message to the Reviewer') }}</label>
